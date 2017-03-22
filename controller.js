0d1e8c55234661792bb074a9a8f8096afdaa2031
OumarKaraga <oumarkaraga@Oumars-MacBook-Pro.local>
2017-03-21 19:16:57 -0500

    minor changes
    updated the controller.js file with Jquery code

--- controller.js
+++ controller.js
@@ -1,6 +1,43 @@
-		function loadMath(){
+$(document).ready(function(){
+		
+		$("#math").click(function(){
 			window.location.href = "mathematics.html";
-		}
-	  	function loadCS(){
+		});
+
+		$("#cs").click(function(){
 			window.location.href = "cs.html";
-		}	
+		});
+
+		$("#physics").click(function(){
+			window.location.href = "physics.html";
+		});
+
+		$("#music").click(function(){
+			window.location.href = "music.html";
+		});
+
+		$("#art").click(function(){
+			window.location.href = "art.html";
+		});
+
+		$("#biology").click(function(){
+			window.location.href = "biology.html";
+		});
+
+		$("#english").click(function(){
+			window.location.href = "english.html";
+		});
+
+		$("#business").click(function(){
+			window.location.href = "business.html";
+		});
+
+		$("#psychology").click(function(){
+			window.location.href = "psychology.html";
+		});
+
+		$("#chemistry").click(function(){
+			window.location.href = "chemistry.html";
+		});
+
+});
